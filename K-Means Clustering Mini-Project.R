d0f092c4a2646d5adde448d6b9c080b8c5aeeb2e
YOUR FULL NAME <SCHULTZRYNE@GMAIL.COM>
2018-11-19 22:03:51 -0500

    Applied clusplot function to excercise 6

--- K-Means Clustering Mini-Project.R
+++ K-Means Clustering Mini-Project.R
@@ -90,3 +90,4 @@ table(wine$Type, fit.km$cluster)
 # Exercise 6:
 # * Visualize these clusters using  function clusplot() from the cluster library
 # * Would you consider this a good clustering?
+clusplot(wine, fit.km$cluster)
